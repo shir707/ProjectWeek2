0a92f3c7ac9ced9b1e19aa3cabe606dfec08547f
Shir Fogel <shirfogel@cs.colman.ac.il>
2022-03-02 16:20:14 +0200

    First commit

---
+++ password-validator.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+PASSWORD=$1
+LEN=`echo ${#PASSWORD}`
+# Set the color variable- we color the output in green if the all the checking are passed and red if didn't
+green='\033[0;32m'
+red='\033[0;31m'
+# Clear the color after that
+clear='\033[0m'
+##checking if the length is less then 10
+if [[ $LEN -lt 10 ]]; then
+echo -e "${red}Password must be of at least 10 characters!${clear}"
+exit 1
+fi
+##this echo returns 0 if all the checkings are passed
+echo $PASSWORD | grep "[a-z]" | grep "[A-Z]" | grep "[0-9]" 
+if [[ $? -ne 0 ]]; then
+echo -e "${red}Password must contain atleast 1 uppercase, lowercase and digits${clear}"
+exit 1
+else
+echo -e "${green}Strong Password${clear}"
+fi
+exit 0
+
